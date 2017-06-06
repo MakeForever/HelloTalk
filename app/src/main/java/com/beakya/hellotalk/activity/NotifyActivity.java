db87162e1a3c5fba69b77e657a42281102e19bc3
박철호 <beakya884@gmail.com>
2017-06-06 23:17:14 +0900

    대화 완전 구현 완료
    1. 백그라운드일때 서버에서 redis에 저장했다가 forground일때 다시 보내는거
    2. fcm notification 성공
    //TODO
    리팩토링 반드시 할것
    gson 라이브러리로 갈아탈것

---
+++ app/src/main/java/com/beakya/hellotalk/activity/NotifyActivity.java
@@ -0,0 +1,15 @@
+package com.beakya.hellotalk.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.beakya.hellotalk.R;
+
+public class notifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notify);
+    }
+}
---
+++ app/src/main/java/com/beakya/hellotalk/services/MyJobService.java
@@ -0,0 +1,8 @@
+package com.beakya.hellotalk.services;
+
+/**
+ * Created by goodlife on 2017. 6. 5..
+ */
+
+public class MyJobServices {
+}
